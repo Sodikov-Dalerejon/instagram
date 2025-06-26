c249427a45fb807da636b2e8ab1f462ca685eeab
Sodikov-Dalerejon <151289581+Sodikov-Dalerejon@users.noreply.github.com>
2025-06-26 13:03:20 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
+  plugins: [react(),tailwindcss()],
+  preview: {
+    host: true,
+    port: process.env.PORT || 4173,
+    allowedHosts: ['https://instagram-1roo.onrender.com']
+  }
 })
